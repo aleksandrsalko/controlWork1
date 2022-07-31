0e5919761f89e7e5eaebbb3c5a56ea0df9918e54
Aleksandr <nord112009@bk.ru>
2022-07-31 13:27:48 +0300

    add task

---
+++ Task/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] firstArray = new string[4] {"1234", "1567", "-2", "computer science"};
+string[] secondArray = new string[firstArray.Length];
+int count = 0;
+
+FillArray(firstArray, secondArray);
+PrintArray(secondArray);
+
+
+void FillArray(string[] firstArray, string[] secondArray)
+{
+  for (int i = 0; i < firstArray.GetLength(0); i++)
+  {
+  if(firstArray[i].Length <= 3)
+    {
+    secondArray[count] = firstArray[i];
+    count++;
+    }
+  }
+}
+
+void PrintArray(string[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    Console.Write($"{array[i]} ");
+  }
+  Console.WriteLine();
+}
